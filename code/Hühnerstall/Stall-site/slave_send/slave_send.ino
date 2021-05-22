867ce566cfc2a5a15a6e3160296068e4bf5024f3
jackwolf24 <ajaygaly@gmail.com>
2021-05-22 14:28:32 +0200

    sd sleepmode-alarm branch inital commit

--- code/Hühnerstall/Stall-site/slave_send/slave_send.ino
+++ code/Hühnerstall/Stall-site/slave_send/slave_send.ino
@@ -1,27 +1,24 @@
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
-#include <SD.h>
 #include <Wire.h>
 #include "RTClib.h"
 #include <avr/sleep.h>
 
 static const PROGMEM u1_t NWKSKEY[] = { 0xBA, 0xC2, 0xD6, 0x4E, 0xDE, 0x38, 0x82, 0x06, 0xB6, 0xC7, 0x92, 0x59, 0x21, 0x4A, 0x30, 0xF0 };
 static const u1_t PROGMEM APPSKEY[] = { 0xA2, 0xC1, 0x76, 0xD1, 0xAE, 0x25, 0xF9, 0xE0, 0x22, 0x99, 0xC0, 0xEF, 0xC0, 0x01, 0x08, 0x9E };
-static const u4_t DEVADDR = 0x26011FDD; //Change this address for every node!
+static const u4_t DEVADDR = 0x26011FDD; // <-- Change this address for every node!
 void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 static osjob_t sendjob;
 
-char received_measurements[20];
+uint8_t received_measurements[20];
 unsigned char last_received;
 int index = 0;
 
 const int alarmPin = 2;
 int statepin = 3;
-
-String DATALOG = "log.txt";
 
 const lmic_pinmap lmic_pins = {
   .nss = 10,
@@ -32,16 +29,13 @@ const lmic_pinmap lmic_pins = {
 
 void setup() {
   Serial.begin(9600);
-
-  while (!SD.begin(3)) {
-    Serial.println("initialization failed");
-  }
-  write_to_log("11Starting", 9, false);
   Wire.begin(5);
   Wire.onReceive(on_receiveEvent);
+  Serial.println(F("Starting"));
+
   pinMode(statepin, INPUT_PULLUP);
 
-  Wire.begin();
+  Wire.begin(); 
 
   os_init();
   LMIC_reset();
@@ -70,40 +64,85 @@ void setup() {
   LMIC_setDrTxpow(DR_SF7, 14);
 
   do_send(&sendjob);
+
+  //enter_sleep();
+}
+void enter_sleep() {
+  sleep_enable();                       // Enabling sleep mode
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);  // Setting the sleep mode, in this case full sleep
+
+  noInterrupts();                       // Disable interrupts
+  attachInterrupt(digitalPinToInterrupt(alarmPin), on_alarm, RISING);
+
+  Serial.println("Going to sleep!");    // Print message to serial monitor
+  Serial.flush();                       // Ensure all characters are sent to the serial monitor
+
+  interrupts();                         // Allow interrupts again
+  sleep_cpu();                          // Enter sleep mode
+
+  /* The program will continue from here when it wakes */
+
+
+  Serial.println("I'm back!");          // Print message to show we're back
+}
+
+
+void on_alarm() {
+  sleep_disable();
+  detachInterrupt(digitalPinToInterrupt(alarmPin));
+  Serial.println("i'm back");
 }
 
 void on_receiveEvent(int howMany) {
-  for (int i = 0; i <= howMany; i++)
+  while (Wire.available())
   {
     last_received = Wire.read();
-    received_measurements[i] = last_received;
+    received_measurements[index] = last_received;
+    index++;
+    Serial.print("receiving: ");
+    Serial.println(last_received);
+  }
+  Serial.println("receiving sens data done, view received_measurements:");
+  for (int i = 0; i <= sizeof(received_measurements) - 1; i++) {
     Serial.println(received_measurements[i]);
   }
-
-  if (int(received_measurements[0]) == 0) {
-    Serial.println("lora send");
-    os_setCallback(&sendjob, do_send);
-    write_to_log(received_measurements, sizeof(received_measurements), true);
-    memset(received_measurements, "", sizeof(received_measurements));
-  }
-
-  else if (int(received_measurements[0] == 1)) {
-    Serial.println("log");
-    write_to_log(received_measurements, sizeof(received_measurements), false);
-    memset(received_measurements, "", sizeof(received_measurements));
-  }
-  else {
-    Serial.println("undefined");
-    write_to_log("11undefined log bool", 20, false);
-  }
+  index = 0;
+  os_setCallback(&sendjob, do_send);
+  
 }
-
 
 void onEvent (ev_t ev) {
   Serial.print(os_getTime());
   Serial.print(": ");
   switch (ev) {
-
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      break;
+    case EV_RFU1:
+      Serial.println(F("EV_RFU1"));
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+      break;
     case EV_TXCOMPLETE:
       Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
       //enter_sleep();
@@ -112,90 +151,45 @@ void onEvent (ev_t ev) {
         Serial.print(F("Data Received: "));
         Serial.write(LMIC.frame + LMIC.dataBeg, LMIC.dataLen);
         Serial.println();
-        write_to_log("11EV_TXCOMPLETE", sizeof("11EV_TXCOMPLETE"), false);
       }
       break;
-
-
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
     case EV_RESET:
       Serial.println(F("EV_RESET"));
-      write_to_log("11EV_RESET", sizeof("11EV_RESET"), false);
       break;
-
     case EV_RXCOMPLETE:
       // data received in ping slot
       Serial.println(F("EV_RXCOMPLETE"));
-      write_to_log("11EV_RXCOMPLETE", sizeof("11EV_RXCOMPLETE"), false);
       break;
-
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
     default:
       Serial.println(F("Unknown event"));
-      write_to_log("11Unknown event", sizeof("11Unknown event"), false);
       break;
   }
 }
 
 void do_send(osjob_t* j) {
+/*
+  int _minutes = 57;
+  int batVoltage = map(1259, 1100, 1400, 0, 255);
+  measurement[0] = _minutes;
+  measurement[1] = batVoltage;
+  measurement[2] = !digitalRead(statepin);
+*/
   if (LMIC.opmode & OP_TXRXPEND) {
     Serial.println(F("OP_TXRXPEND, not sending"));
-    write_to_log("11OP_TXRXPEND, not sending", sizeof("11OP_TXRXPEND, not sending"), false);
-
   } else {
     LMIC_setTxData2(1, (uint8_t*) received_measurements, sizeof(received_measurements), 0);
     Serial.println(F("Packet queued"));
-    write_to_log("11Packet queued", sizeof("Packet queued"), false);
-  }
-}
-
-void write_to_log(char content[], int _sizeof, bool data) {
-  File outputFile = SD.open("log_data.txt", FILE_WRITE);
-
-  if (outputFile) {
-    outputFile.seek(EOF);
-
-    for (int i = 1; i <= _sizeof; i++) {
-
-      if (data) {
-        switch (i) {
-          case 1:
-            outputFile.print("device: 0 ");
-          case 2:
-            outputFile.print("doorstate: ");
-            break;
-          case 3:
-            outputFile.print("voltage: ");
-            break;
-          case 6:
-            outputFile.print("uptime:");
-            break;
-          case 7:
-            outputFile.print("time: ");
-            break;
-        }
-        outputFile.print(content[i]);
-        outputFile.print(" ");
-      }
-      else {
-        if (i == 1) {
-          outputFile.print("device: ");
-          outputFile.print(int(content[1]));
-          outputFile.print(" ");
-        }
-        else {
-          outputFile.print(content[i]);
-        }
-      }
-      //Serial.print(content[i]);
-    }
-    outputFile.println(" ");
-    //Serial.println("");
-    Serial.println("writing to log");
-    outputFile.close();
   }
 
-  else {
-    Serial.println("error opening datalog.txt");
-  }
 }
 void loop() {
   os_runloop_once();
