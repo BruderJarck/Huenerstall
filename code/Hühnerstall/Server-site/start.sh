0d6af8c936e3cc895306c7d6d2615e8653f7dc6e
jackwolf24 <ajaygaly@gmail.com>
2021-05-22 12:43:08 +0200

    fitted to new location

--- code/Hühnerstall/Server-site/start.sh
+++ code/Hühnerstall/Server-site/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Starting Flask example app.
+echo Starting Hühnerstall Server.
 cd /home/pi/Huenerstall/venv/bin
 . activate
-cd /home/pi/Huenerstall/code/test/'local web'
-gunicorn -w 2 -b 192.168.2.110:8083 testflask_mqtt:app
+cd /home/pi/Huenerstall/Hühnerstall/Server-site
+gunicorn -w 2 -b 192.168.2.110:8083 hühnerstall-server:app
